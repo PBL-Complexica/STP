6833e8e5917f1bc7d4087fc8628b82adf1749961
SexomQ <63875270+SexomQ@users.noreply.github.com>
2023-10-08 13:34:12 +0300

    set jwt protection
    added the /protected route that check the jwt if valid

--- src/back/app.py
+++ src/back/app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migratex
+from flask_migrate import Migrate
 
 import jwt
 from flask_jwt_extended import create_access_token
@@ -46,7 +46,9 @@ def signup():
         phone_number = data['phone_number']
         password = data['password']
 
-        # TODO: Get the user data from database
+                        # TODO: [Get the user data from database]
+        # --- Just for testing, we will pass user data directly
+        user.phone_number = phone_number
 
         # Check if the phone number is already registered
         if phone_number in user.phone_number:
@@ -55,6 +57,9 @@ def signup():
         # # Hash the password securely (you should use a proper password hashing library)
         # hashed_password = hashlib.sha256(salted_password.encode()).hexdigest()
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        
+        # --- Just for testing, we will pass user data directly
+        user.password_hash = hashed_password
 
         # Store the user data in the local "database"
         user.first_name = data['first_name']
@@ -67,12 +72,18 @@ def signup():
 
 
 # Endpoint for user login
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
 
     phone_number = data['phone_number']
     password = data['password']
+
+    # TODO: [Get the user data from database]
+    # --- Just for testing, we will pass user data directly
+    user.phone_number = phone_number
 
     # Check if the phone number is registered
     if phone_number not in user.phone_number:
@@ -81,11 +92,26 @@ def login():
     # Hash the password securely (you should use a proper password hashing library)
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
+    # --- Just for testing, we will pass user data directly
+    user.password_hash = hashed_password
+
     # Check if the provided password is correct
-    if hashed_password != users[phone_number]['password']:
+    if hashed_password != user.password_hash:
         return jsonify({'message': 'Incorrect password'}), 401
 
-    return jsonify({'message': 'Logged in successfully', 'user_id': users[phone_number]['user_id']}), 200
+    access_token = create_access_token(identity=phone_number)
+    return jsonify(access_token=access_token)
+    # return jsonify({'message': 'Logged in successfully', 'user_id': users[phone_number]['user_id']}), 200
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 
 if __name__ == '__main__':
