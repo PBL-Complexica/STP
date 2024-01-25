7aaa6660ed6f1ab4612fd3a5dcaade2bde9d10cd
muffindud <ccatlabuga2002@gmail.com>
2024-01-25 17:06:56 +0200

    remove ssl context

--- src/back/app.py
+++ src/back/app.py
@@ -1,22 +1,12 @@
-import hashlib
-import requests
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
 
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import create_refresh_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
-from user import *
 from Database import Database
-
-# import OTP.OTPGeneration as OTP
-# from flask-mail import Mail, Message
 
 app = Flask(__name__)
 
@@ -25,7 +15,6 @@ db = Database()
 # Load environment variables
 load_dotenv()
 
-
 # Set up the Flask-JWT-Extended extension
 app.config["JWT_SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
@@ -33,9 +22,8 @@ app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
 jwt = JWTManager(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-user = User()
-
 if __name__ == '__main__':
     import routes
     routes.init()
-    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context='adhoc')
+    # app.run(debug=True, host='0.0.0.0', port=5000, ssl_context='adhoc')
+    app.run(debug=True, host='0.0.0.0', port=5000)
